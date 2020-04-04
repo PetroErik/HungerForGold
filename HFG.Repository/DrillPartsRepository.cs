87cef8586f96caee0a206b070828c21c06d513fe
petroerik <petroerik1998@gmail.com>
2020-04-04 11:49:47 +0200

    Created IRepository and all its necessary parts.

--- HFG.Repository/DrillPartsRepository.cs
+++ HFG.Repository/DrillPartsRepository.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace HFG.Repository
 {
-    public class DrillPartsRepository
+    public class DrillPartsRepository : IRepository
     {
+        int ActualStorageLvl = Config.StorageLvl1;
+        int ActualFuelTankLvl = Config.FuelTankLvl1;
+        int AtualDrillLvl = Config.DrillLvl1;
+
+        int TotalPoints;
+        int ActualPoints;
+        int StorageFullness;
+
+        public int calcTotalPoints(int actualPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void clearStorage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void collectBricks(BricksType brick)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void moveDrill(Direction d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int upgradeDrill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int upgradeFuelTank()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int upgradeStorage()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
